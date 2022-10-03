2e18e11bc052552e9516683dec8f2f007ac57c31
LSW <sswon314@naver.com>
2022-10-03 20:30:51 +0900

    fix header layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,22 +12,17 @@ import { BsQuestionCircle, BsPersonSquare, BsHouseDoor } from "react-icons/bs";
 // └헤더 하단바(페이지별에 따라 다른 색상구성)
 
 const Header = function () {
-    const [loginStatus, setLoginStatus] = useState(false);
-    const gotoHome = () => {
-        alert("홈으로");
-    }
-    const requestLogin = () => {
-        setLoginStatus(!loginStatus);
-    }
     return (
         <div>
             <Row className="header-top px-5">
-                <Col className="header-logo" md={3} onClick={gotoHome}></Col>
+                <Col className="header-logo" md={3}>
+                    <Link to="/home"></Link>
+                </Col>
                 <Col className="align-self-center" md={{ span: 3, offset: 6 }}>
                     <Stack direction="horizontal" gap={4}>
                         <Link to="/faq" className="ms-auto"><BsQuestionCircle size="32" /></Link>
-                        <Link className="header-login" onClick={requestLogin}>{loginStatus ? "로그아웃" : "로그인"}</Link>
-                        <Link ><BsPersonSquare size="32" /></Link>
+                        <Link to="/login" className="header-login">로그인</Link>
+                        <Link to="/myinfo"><BsPersonSquare size="32" /></Link>
                     </Stack>
                 </Col>
 
@@ -36,7 +31,7 @@ const Header = function () {
             {/* 여기 부분은 페이지에 따라서 배경색상과 글자 색을 바꿔줘야함 */}
             <Row className="header-bottom color-first px-5">
                 <Col className="header-homelink" md={3}>
-                    <Link to="/">
+                    <Link to="/home">
                         <Stack direction="horizontal" gap={3}>
                             <BsHouseDoor size="32" />
                             <div>홈으로 가기</div>
