f5fee2999bebe0211abcebeb7b81b482a8fd6736
LSW <sswon314@naver.com>
2022-10-15 01:47:25 +0900

    설문 결과 페이지 UI 수정

--- src/components/SurveyResult/StatisticCards/SubjectiveStat.js
+++ src/components/SurveyResult/StatisticCards/SubjectiveStat.js
@@ -1,18 +1,23 @@
 // 응답 카드 컴포넌트(주관식)
 
-import { Col } from "react-bootstrap";
+import { Col, Table } from "react-bootstrap";
 
 const SubjectiveStat = function ({ statCard }) {
     return (
         <div className="survey-result-stat">
             <Col className="mb-5 fs-4">Q{statCard.questionOrder}. {statCard.title}</Col>
             <div className="survey-result-subjectiveAnswer mb-4 fs-5">
-                {
-                    statCard.answers.map((answer, answerIdx) => {
-                        return <div className="py-1" key={answerIdx}>{answer.value} < br /></div>
-                    })
-                }
+                <Table striped bordered hover>
+                    <tbody  >
+                        {
+                            statCard.answers.map((answer, answerIdx) => {
+                                return <tr className="py-1" key={answerIdx}><td>{answer.value}</td></tr>
+                            })
+                        }
+                    </tbody>
+                </Table>
             </div>
+
         </div >
     )
 }
