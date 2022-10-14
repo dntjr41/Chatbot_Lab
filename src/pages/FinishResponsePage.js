cd4846a0392cb6bc1c4f56ea7aa7f3ee022981bd
LSW <76082792+sswon314@users.noreply.github.com>
2022-10-15 01:34:13 +0900

    페이지별 UI수정, State구조 변경 (#11)
    * nivo 패키지 추가
    
    * 설문 제작 페이지 UI, State 변경
    
    * 설문 응답 페이지 UI, State 변경
    
    * 설문 결과 페이지 UI

--- src/pages/FinishResponsePage.js
+++ src/pages/FinishResponsePage.js
@@ -9,17 +9,10 @@ import '../css/FinishResponsePage.css'
 // └응답완료 컴포넌트
 //   └감사인사, 수정링크, 메인페이지 링크 버튼
 
-// 응답 페이지에서
-
 const FinishResponsePage = function () {
     // 홈페이지로 이동
     const gotoHome = () => {
         alert("홈 페이지로 이동");
-    }
-
-    // 설문창 닫기
-    const closeWindow = () => {
-        alert("창 닫기");
     }
 
     // 이전 페이지로 돌아가서 응닫 수정하기
@@ -31,14 +24,14 @@ const FinishResponsePage = function () {
         <div className="finish-response-layout">
             <Header />
             <div className="finish-response-thankBox">
-                <Row style={{ marginBottom: "120px", fontWeight: "600" }}>
+                <Row style={{ marginBottom: "120px", fontSize: "24px", fontWeight: "600" }}>
                     <Col>설문에 응답해주셔서 감사합니다.</Col>
                 </Row>
                 <Row style={{ marginBottom: "65px" }}>
-                    <Link to="/response"><Col>응답 수정하기</Col></Link>
+                    <Link to="/response" onClick={backToResponse}>응답 수정하기</Link>
                 </Row>
                 <Row>
-                    <Col><button className="finish-response-exit" type="button">SurMoonVey알아보기</button></Col>
+                    <Link to="/home"><button className="finish-response-exit" type="button" onClick={gotoHome}>SurMoonVey알아보기</button></Link>
                 </Row>
             </div>
         </div>
